1e43a4923c8628c99ae0fd711d21b36a5ec4ef2f
k8ie <k8ie@mcld.eu>
2024-03-08 19:57:19 +0100

    Properly use the "status" field

--- sql/V0.0.0.2__initial_data.sql
+++ sql/V0.0.0.2__initial_data.sql
@@ -3,7 +3,7 @@ INSERT INTO `kategorie` (name,path_name,parent_id) VALUES
 ('Ale jednoduchý', 'ale_jednoduchy', 1);
 
 INSERT INTO `uzivatele` (user_id,status,username,password,bio) VALUES
-('nktrjsk', 'away', 'nktrjsk', '0b72bc75726742d347d9e8e182234c4cb42e630b6c6b3a264a3491d51f921f55$622ed8d05081ebcbd535c0dde0fa2dac414cbad05d76f18f13b2e06b377a75d1', 'can\'t write SQL');
+('nktrjsk', 'user', 'nktrjsk', '0b72bc75726742d347d9e8e182234c4cb42e630b6c6b3a264a3491d51f921f55$622ed8d05081ebcbd535c0dde0fa2dac414cbad05d76f18f13b2e06b377a75d1', 'can\'t write SQL');
 
 INSERT INTO `clanky` (category_id,created,updated,autor,nadpis,text,sources) VALUES
 (NULL, 0, 0, (SELECT id FROM uzivatele WHERE user_id='nktrjsk'), 'sadkjewqjtklewqjtklewq', '', ''),
